1d23ca7362f6fa8eda93374173994b7f150d808b
ryosuke_doi <ryo-62-gnt-0000@outlook.jp>
2022-06-14 20:05:18 +0900

    javascripttask2_question

---
+++ calculator.js
@@ -0,0 +1,32 @@
+'use strict';
+let display = document.getElementById("display");
+
+function btn_click(target){
+  let number = target.innerHTML; 
+  if (number == "=") {
+    display.innerHTML = eval(display.innerHTML) //displayに映っている数字で演算
+    } else if(number == "AC") {
+      display.innerHTML = "0" //AC押した際にdisplayを0に
+    } else {
+      if (display.innerHTML == "0") {
+        display.innerHTML = number //displayが0表示の時、押したbtnを表示
+      } else {
+        display.innerHTML += number //displayの数字+numberをdisplayに加える
+      }
+    }
+}
+
+function ope_click(operator){
+  var operator = operator.innerHTML;
+  var display_ope = display.innerHTML.slice(0,-1);
+  let slice = display.innerHTML.slice(-1);
+
+  if ((slice != "+")||(slice != "-")||(slice != "*")||(slice != "/")) {
+    display.innerHTML += operator;//displayにdisplayに入力されているものと演算子を加える
+  }else if ((slice == "+")||(slice == "-")||(slice == "*")||(slice == "/")){
+    display.innerHTML = display_ope + operator; //演算子を上書き
+  }else{
+    ;
+  }
+
+}
